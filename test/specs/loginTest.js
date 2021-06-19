bfd6610c0515b5651508c041a5464eb150efc036
allanocosta <73409778+allanocosta@users.noreply.github.com>
2021-06-19 16:42:35 -0300

    Update loginTest.js

--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -1,22 +1,23 @@
 const assert = require('assert');
 
 describe('My Account test', () => {
-    before('Should login with valid email and password', async () => {
+    before('Should access home page and validate', async () => {
         const modal_newsletter = await $("div#newsletter div.lightbox__modal");
         const btn_closeNewsletter = await $("div#newsletter button[class*='close lightbox']");
         
-        const myAccount_btn_loginWithEmailAndPassword = await $("button#loginWithUserAndPasswordBtn");
-
         await browser.url('https://loja.electrolux.com.br/');
         await btn_closeNewsletter.click();
         await expect(modal_newsletter).not.toBeDisplayed();
         await expect(browser).toHaveTitle('Ofertas Eletro: geladeira, aspirador, fogão e mais | Electrolux');
-        
-        
     });
     
-    it('', async () => {
-        
+    it('Should login with valid email and password', async () => {
+        const myAccount_modal_vtexAuthSelector = await $("div#vtexIdUI-auth-selector");
+        const myAccount_btn_loginWithEmailAndPassword = await $("button#loginWithUserAndPasswordBtn");
+        const myAccount_modal_classicLogin = await $("form#vtexIdUI-form-classic-login");
+        const myAccount_input_email = await $("input#inputEmail");
+        const myAccount_input_password = await $("input#inputPassword");
+        const myAccount_btn_classicLogin = await $("div#vtexIdUI-classic-login button#classicLoginBtn");
     });
 });
 
