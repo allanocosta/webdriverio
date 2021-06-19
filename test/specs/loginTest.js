8e44d666523449ab3ab5f62379b46550b8946a78
allanocosta <73409778+allanocosta@users.noreply.github.com>
2021-06-19 16:16:58 -0300

    start test electrolux
    Starting the automation project Electrolux BR with WebdriverIO

--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -1,22 +1,22 @@
 const assert = require('assert');
 
-// const LoginPage = require('../pageobjects/login.page');
-// const SecurePage = require('../pageobjects/secure.page');
-
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        // await LoginPage.open();
-        
-        /* await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!'); */
+describe('My Account test', () => {
+    before('Should login with valid email and password', async () => {
         const modal_newsletter = await $("div#newsletter div.lightbox__modal");
         const btn_closeNewsletter = await $("div#newsletter button[class*='close lightbox']");
         
+        const myAccount_btn_loginWithEmailAndPassword = await $("button#loginWithUserAndPasswordBtn");
+
         await browser.url('https://loja.electrolux.com.br/');
         await btn_closeNewsletter.click();
         await expect(modal_newsletter).not.toBeDisplayed();
         await expect(browser).toHaveTitle('Ofertas Eletro: geladeira, aspirador, fogão e mais | Electrolux');
+        
+        
+    });
+    
+    it('', async () => {
+        
     });
 });
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -28,7 +28,7 @@ exports.config = {
     ],
     // Patterns to exclude.
     exclude: [
-        // 'path/to/excluded/files'
+        './test/specs/**/*.e2e.js'
     ],
     //
     // ============
@@ -100,11 +100,11 @@ exports.config = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: (10 * 1000),
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
-    connectionRetryTimeout: 120000,
+    connectionRetryTimeout: (12 * 1000),
     //
     // Default request retries count
     connectionRetryCount: 3,
@@ -144,7 +144,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: (60 * 1000)
     },
     //
     // =====
